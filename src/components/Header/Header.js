7f78969b5a2209e8a31fb1eb635072fb085c5d33
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-10-03 18:58:22 -0400

    added Header and BookSearch components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <section className="header-section">
+            <div>
+                <h1>Google Book Search</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
